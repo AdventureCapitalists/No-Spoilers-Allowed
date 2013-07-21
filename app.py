8daa350cdea6c112cfa30f7afb01f4fc9ab538d9
Rob Spectre <rob.spectre@gmail.com>
2013-07-21 17:47:37 -0400

    Draft of Twilio app.

--- app.py
+++ app.py
@@ -35,51 +35,10 @@ def twilio_secure(func):
 @twilio_secure
 def voice():
     response = twiml.Response()
-    response.say("Congratulations! You deployed the Twilio Hackpack" \
-            " for Heroku and Flask.")
+    response.play("/static/sounds/nospoilersallowed.mp3")
+    response.say("We are always listening.")
+    response.record()
     return str(response)
-
-
-# SMS Request URL
-@app.route('/sms', methods=['GET', 'POST'])
-@twilio_secure
-def sms():
-    response = twiml.Response()
-    response.sms("Congratulations! You deployed the Twilio Hackpack" \
-            " for Heroku and Flask.")
-    return str(response)
-
-
-# Twilio Client demo template
-@app.route('/client')
-def client():
-    configuration_error = None
-    for key in ('TWILIO_ACCOUNT_SID', 'TWILIO_AUTH_TOKEN', 'TWILIO_APP_SID',
-            'TWILIO_CALLER_ID'):
-        if not app.config[key]:
-            configuration_error = "Missing from local_settings.py: " \
-                    "%s" % key
-            token = None
-    if not configuration_error:
-        capability = TwilioCapability(app.config['TWILIO_ACCOUNT_SID'],
-            app.config['TWILIO_AUTH_TOKEN'])
-        capability.allow_client_incoming("joey_ramone")
-        capability.allow_client_outgoing(app.config['TWILIO_APP_SID'])
-        token = capability.generate()
-    params = {'token': token}
-    return render_template('client.html', params=params,
-            configuration_error=configuration_error)
-
-
-# Installation success page
-@app.route('/')
-def index():
-    params = {
-        'Voice Request URL': url_for('.voice', _external=True),
-        'SMS Request URL': url_for('.sms', _external=True),
-        'Client URL': url_for('.client', _external=True)}
-    return render_template('index.html', params=params,
-            configuration_error=None)
 
 
 def validate_twilio_request():
