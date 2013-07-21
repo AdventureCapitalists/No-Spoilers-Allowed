39e36a061555c92da3b87a2ab676ad3675f01819
Rob Spectre <rob.spectre@gmail.com>
2013-07-21 19:31:01 -0400

    Add app debug.

--- app.py
+++ app.py
@@ -72,4 +72,5 @@ if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     if port == 5000:
         app.debug = True
+    app.debug = True
     app.run(host='0.0.0.0', port=port)
