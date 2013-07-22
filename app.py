fd721679307710ec5ec19a0bee6349d9c857ee28
Rob Spectre <rob.spectre@gmail.com>
2013-07-21 23:34:51 -0400

    Remove broken request validation code.

--- app.py
+++ app.py
@@ -20,43 +20,14 @@ app = Flask(__name__, static_url_path='/static')
 app.config.from_pyfile('local_settings.py')
 
 
-def twilio_secure(func):
-    """Wrap a view function to ensure that every request comes from Twilio."""
-    @wraps(func)
-    def wrapper(*a, **kw):
-        if validate_twilio_request():
-            return func(*a, **kw)
-        return Response("Not a valid Twilio request", status=403)
-    return wrapper
-
-
 # Voice Request URL
 @app.route('/voice', methods=['GET', 'POST'])
-#@twilio_secure
 def voice():
     response = twiml.Response()
     response.play("/static/sounds/nospoilersallowed.mp3")
     response.say("We are always listening.")
     response.record()
     return str(response)
-
-
-def validate_twilio_request():
-    """Ensure a request is coming from Twilio by checking the signature."""
-    validator = RequestValidator(current_app.config['TWILIO_AUTH_TOKEN'])
-    if 'X-Twilio-Signature' not in request.headers:
-        return False
-    signature = request.headers['X-Twilio-Signature']
-    if 'CallSid' in request.form:
-        # See: http://www.twilio.com/docs/security#notes
-        url = URLObject(url_for('.voice', _external=True)).without_auth()
-        if request.is_secure:
-            url = url.without_port()
-    elif 'SmsSid' in request.form:
-        url = url_for('.sms', _external=True)
-    else:
-        return False
-    return validator.validate(url, request.form, signature)
 
 
 # Handles SIGTERM so that we don't get an error when Heroku wants or needs to
